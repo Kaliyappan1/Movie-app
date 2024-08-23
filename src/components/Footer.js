70a8a67665fa1df207a5b9ef281ecfb054498b97
kaliyappan <yappank17@gmail.com>
2024-08-23 14:21:03 +0530

    header and footer in laptop size changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2'>
+     <div className='flex items-center justify-center gap-4'>
+     <a target='_blank' href="https://kaliyappan-r.web.app/">About</a>
+      <a target='_blank' href="mailto:kaliyappanr.tech@gmail.com">Contact</a>
+     </div>
+     <p className='text-sm'>Created by Dynamic Coding with Kaliyappan</p>
+    </footer>
   )
 }
 
